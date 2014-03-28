991064d347e74499c751d0d1d2c1f51261b6b401
Pascal Fischli <pfischli8@gmail.com>
2014-03-28 18:01:12 +0100

    Changed the method descriptions to XML style

--- vfs/vfs.core/IJCDBasicVFS.cs
+++ vfs/vfs.core/IJCDBasicVFS.cs
@@ -3,61 +3,80 @@ namespace vfs.core
     public interface IJCDBasicVFS
     {
 
-        // Create a new VFS-file.
-        // Exceptions:
-        // - no such path.
+        /// <summary>
+        /// Create a new VFS-file.
+        /// </summary>
+        /// <param name="hfsPath">The path of the file on the hard file system</param>
+        /// <param name="size">The size of the new vfs</param>
+        /// <returns>An object of an implementation of the IJCDBasicVFS interface</returns>
+        /// <exception cref="System.IO.DirectoryNotFoundException"
         // - too little space available on HFS
         // - invalid path string (file name too long/invalid characters).
         // - no permissions to write on HFS.
         static IJCDBasicVFS Create(string hfsPath, ulong size);
 
-        // Delete an unmounted VFS from HFS.
-        // Exceptions:
+        /// <summary>
+        /// Delete an unmounted VFS from HFS.
+        /// </summary>
+        /// <param name="hfsPath">The path of the file on the host file system</param>
+        /// <exception cref="System.IO.DirectoryNotFoundException"
         // - VFS is mounted.
-        // - invalid path string (file name too long/invalid characters).
-        // - no such path.
         static void Delete(string hfsPath);
 
-        // Mount an existing VFS-file.
-        // Return: VFS id.
-        // Exceptions:
+        /// <summary>
+        /// Mount an existing VFS-file.
+        /// </summary>
+        /// <param name="hfsPath"> The path of the file on the host file system</param>
+        /// <returns>An object of an implementation of the IJCDBasicVFS interface</returns>
+        /// <exception cref="System.IO.DirectoryNotFoundException"></exception>
         // - VFS file already mounted.
         // - file is not VFS type.
-        // - invalid path string (file name too long/invalid characters).
         static IJCDBasicVFS Open(string hfsPath);
 
-        // Unmount a mounted VFS.
-        // Exceptions:
+        /// <summary>
+        /// Unmount a mounted VFS.
+        /// </summary>
+        // - No VFS mounted
         void Close();
 
-        // Get total size of a mounted VFS.
-        // Return: Size of mounted VFS.
-        // Exceptions:
+        /// <summary>
+        /// Get total size of a mounted VFS.
+        /// </summary>
+        /// <returns>Size of mounted VFS.</returns>
+        // - No VFS mounted
         ulong Size();
 
-        // Get total amount of occupied space on a mounted VFS.
-        // Return: Amount of occupied space.
-        // Exceptions:
-        // Notes:
-        // - It should hold that OccupiedSpace + UnoccupiedSpace == Size
+        /// <summary>
+        /// Get total amount of occupied space on a mounted VFS.
+        /// </summary>
+        /// <returns>Amount of occupied space.</returns>
+        /// <remarks>It should hold that OccupiedSpace + UnoccupiedSpace == Size</remarks>
+        // - No VFS mounted
         ulong OccupiedSpace();
 
-        // Get total amount of unoccupied space on a mounted VFS.
-        // Return: Amount of unoccupied space.
-        // Exceptions:
-        // Notes:
-        // - It should hold that OccupiedSpace + UnoccupiedSpace == Size
+        /// <summary>
+        /// Get total amount of unoccupied space on a mounted VFS.
+        /// </summary>
+        /// <returns>Amount of unoccupied space.</returns>
+        /// <remarks>It should hold that OccupiedSpace + UnoccupiedSpace == Size</remarks>
+        /// - No VFS mounted
         ulong FreeSpace();
 
-        // Create a directory on a mounted VFS.
-        // Optionally create parents (like mkdir -p).
+        /// <summary>
+        /// Create a directory on a mounted VFS.
+        /// Optionally create parents (like mkdir -p).
+        /// </summary>
+        /// <param name="vfsPath"></param>
+        /// <param name="createParents"></param>
         // Exceptions:
         // - no such path (createParents == false).
         // - too little space available on VFS.
         // - invalid path string (file name too long/invalid characters).
         void CreateDirectory(string vfsPath, bool createParents);
 
-        // Import a file or directory from HFS to a mounted VFS.
+        /// <summary>
+        /// Import a file or directory from HFS to a mounted VFS.
+        /// </summary>
         // Exceptions:
         // - no such path on HFS.
         // - no such path on VFS.
@@ -65,7 +84,9 @@ namespace vfs.core
         // - invalid VFS path string (file name too long/invalid characters).
         void ImportFile(string hfsPath, string vfsPath);
 
-        // Export a file or directory from a mounted VFS to HFS.
+        /// <summary>
+        /// Export a file or directory from a mounted VFS to HFS.
+        /// </summary>
         // Exceptions:
         // - no such path on VFS.
         // - no such path on HFS.
@@ -74,29 +95,37 @@ namespace vfs.core
         // - invalid VFS path string (file name too long/invalid characters).
         void ExportFile(string vfsPath, string hfsPath);
 
-        // Delete a file or directory on a mounted VFS.
+        /// <summary>
+        /// Delete a file or directory on a mounted VFS.
+        /// </summary>
         // Exceptions:
         // - invalid path string (file name too long/invalid characters).
         // - no such path.
         // - path points to a directory (recursive == false).
         void DeleteFile(string vfsPath, bool recursive);
 
-        // Rename file or directory on a mounted VFS.
+        ///<summary>
+        /// Rename file or directory on a mounted VFS.
+        /// </summary>
         // Exceptions:
         // - invalid path string (file name too long/invalid characters).
         // - invalid file name (too long/invalid characters).
         // - no such file on VFS.
         void RenameFile(string vfsPath, string newName);
 
-        // Move file or directory on a mounted VFS.
+        /// <summary>
+        /// Move file or directory on a mounted VFS.
+        /// </summary>
         // Exceptions:
         // - invalid path string (file name too long/invalid characters).
         // - invalid file name (too long/invalid characters).
         // - no such file on VFS.
         void MoveFile(string vfsPath, string newVfsPath);
 
-        // List contents of a directory.
-        // Return: List of directories and files contained in vfsPath.
+        /// <summary>
+        /// List contents of a directory.
+        /// </summary>
+        /// <returns>List of directories and files contained in vfsPath.</returns>
         // Exceptions:
         // - path points to a file (not directory).
         JCDFile[] ListDirectory(string vfsPath);
