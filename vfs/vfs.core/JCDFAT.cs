184119aff845274019d9ea56bfa888d13f1609a7
Michael Bang <michael@katnegermis.dk>
2014-03-28 15:31:52 +0100

    mr Grozmann

--- vfs/vfs.core/JCDFAT.cs
+++ vfs/vfs.core/JCDFAT.cs
@@ -5,7 +5,6 @@ namespace vfs.core
     public class JCDFAT : IJCDBasicVFS
     {
         private bool initialized = false;
-        private bool mounted = false;
 
         // All sizes in this class are given in bytes.
         private int numMetaDataBlocks = 1; // Number of blocks used for meta data' (doesn't include the FAT)
@@ -24,7 +23,12 @@ namespace vfs.core
 
         private FileStream fs;
 
-        public static void Create(string hfsPath, ulong size)
+        private JCDFAT(FileStream fs, ulong size)
+        {
+            Init(size);
+        }
+
+        public static JCDFAT Create(string hfsPath, ulong size)
         {
             // Make sure the directory exists.
             if (File.Exists(Path.GetDirectoryName(hfsPath))) {
@@ -39,44 +43,37 @@ namespace vfs.core
             // Create fsfile.
             try {
                 var fs = new FileStream(hfsPath, FileMode.CreateNew, FileAccess.ReadWrite, FileShare.None);
-                Init(size);
+                return new JCDFAT(fs, size);
             } catch (IOException) {
                 // The possibly already exists or the stream has been unexpectedly closed
                 throw new FileAlreadyExistsException();
             }
         }
 
-        public void Mount(string hfsPath)
+        public static JCDFAT Open(string hfsPath)
         {
             // Throws FileNotFoundException.
-            this.fs = new FileStream(hfsPath, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
-            Mount(this.fs);
+            var fs = new FileStream(hfsPath, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            // parse meta data
+            ulong size = 0L;
+            return new JCDFAT(fs, size);
         }
 
-        private void Mount(FileStream fs)
+        public void Close()
         {
-            // Parse meta data from fs.
-            // Init(size);
-            // Set this.rootDirectory and this.currentDirectory.
-            // Initialize this.fat. At least read in root directory block.
-            this.mounted = true;
-        }
-
-        public void Unmount()
-        {
-            if (!this.mounted) {
-                throw new FileSystemNotMounted();
-            }
-
             this.fs.Flush();
             this.fs.Dispose();
         }
 
         private void Init(ulong size)
         {
+            if (size >= 1 << 46)
+            {
+                throw new FileTooFuckingBigException();
+            }
             this.size = size;
             this.filesPerBlock = this.blockSize / this.fileEntrySize;
-            this.numBlocks = this.size / this.blockSize; // Should round down.
+            this.numBlocks = this.size / ulong.Parse(this.blockSize); // Should round down.
             this.fatSize = this.numDataBlocks; // Each block takes up 1 byte.
             this.numDataBlocks = this.numBlocks - this.numMetaDataBlocks;
             this.fat = new int[this.numDataBlocks];
