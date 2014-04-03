453461ba3e7835a3c1aca2486324a59a5179792b
Michael Bang <michael@katnegermis.dk>
2014-04-03 14:04:52 +0200

    Add files missing in last commit.

---
+++ vfs/vfs.core/Helpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vfs.core
+{
+    class Helpers
+    {
+        /// <summary>
+        /// Round up integer divison
+        /// </summary>
+        /// <param name="num">Numerator</param>
+        /// <param name="den">Denominator</param>
+        /// <returns></returns>
+        public static ulong ruid(ulong num, ulong den)
+        {
+            return (num + den - 1) / den;
+        }
+    }
+}
