b976d5b516cd15b66712dc1a280102f0940b8717
Michael Bang <michael@katnegermis.dk>
2014-04-06 20:53:41 +0200

    Make sure that FileDeleterVisitor doesn't delete root dir and search file.

--- vfs/vfs.core/visitor/FileDeleterVisitor.cs
+++ vfs/vfs.core/visitor/FileDeleterVisitor.cs
@@ -10,6 +10,13 @@ namespace vfs.core.visitor
     {
         public bool Visit(JCDFAT vfs, uint block)
         {
+            // Make sure that we don't delete any reserved blocks.
+            // This should probably be fixed somewhere else, but I guess
+            // this is a nice safety mechanism to begin with.
+            if (block < JCDFAT.reservedBlockNumbers) {
+                return true;
+            }
+
             vfs.FatSetFree(block);
             return true;
         }
