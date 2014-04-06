619362187cc57fc46e778477f0f3a52671207eba
Michael Bang <michael@katnegermis.dk>
2014-04-06 13:15:42 +0200

    Set exceptions to namespace vfs.exceptions.

--- vfs/vfs.core/exceptions/NotEnoughSpaceException.cs
+++ vfs/vfs.core/exceptions/NotEnoughSpaceException.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace vfs.core.exceptions
+namespace vfs.exceptions
 {
     class NotEnoughSpaceException : Exception
     {
