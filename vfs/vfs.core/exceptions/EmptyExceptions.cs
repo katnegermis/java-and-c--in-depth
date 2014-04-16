87bbd6385fd5cf771bacf686645a3a240626c7db
Vladimir Grozman <xogede@gmail.com>
2014-04-16 18:01:00 +0200

    added EmptyExceptions.cs

---
+++ vfs/vfs.core/exceptions/EmptyExceptions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace vfs.exceptions
+{
+    public class FileAlreadyExistsException : Exception { }
+
+    public class FileNotFoundException : Exception { }
+
+    public class ParentNotFoundException : FileNotFoundException { }
+
+    public class NotAFolderException : FileNotFoundException { }
+
+    public class FileExistsException : Exception { }
+
+    public class MoveToSelfException : Exception { }
+
+    public class FileSystemNotOpenException : Exception { }
+
+    public class InvalidFileException : Exception { }
+
+    public class InvalidFileNameException : Exception { }
+
+    public class InvalidSizeException : Exception { }
+
+    public class NotEnoughSpaceException : Exception { }
+
+    public class RootDeletionException : Exception { }
+
+    public class NonRecursiveDeletionException : Exception { }
+
+    public class InvalidFATIndexException : Exception { }
+
+    public class ReachedEndOfFileException : Exception { }
+}
