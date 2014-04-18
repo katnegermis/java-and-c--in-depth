2823e952a8febf9794f3fa68057f70c4dc3f4756
Michael Bang <michael@katnegermis.dk>
2014-04-18 17:32:51 +0200

    Add file missing from renaming in previous commit.

---
+++ vfs/vfs.core.indexing/IndexedFile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vfs.core;
+
+namespace vfs.core.indexing {
+    [Serializable()]
+    public class IndexedFile {
+        // Name isn't _really_ necessary since we use it as key.
+        public string Name;
+        public string Path;
+
+        public IndexedFile(string name, string path) {
+            this.Name = name;
+            this.Path = path;
+        }
+
+        public override bool Equals(object obj) {
+            if (obj == null || !(obj is IndexedFile)) {
+                return false;
+            }
+            return this.Name == ((IndexedFile)obj).Name && this.Path == ((IndexedFile)obj).Path;
+        }
+
+        public override int GetHashCode() {
+            return base.GetHashCode();
+        }
+
+        public static bool operator ==(IndexedFile f1, IndexedFile f2) {
+            return f1.Equals(f2);
+        }
+
+        public static bool operator !=(IndexedFile f1, IndexedFile f2) {
+            return !(f1 == f2);
+        }
+    }
+}
