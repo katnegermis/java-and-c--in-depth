170f8c5d882d29fc3363b5f21fe27b25ee29a1ab
Michael Bang <michael@katnegermis.dk>
2014-04-23 22:16:23 +0200

    Add JCDFileStream class, and accompanying tests.

---
+++ vfs/vfs.core.tests/JCDFileStreamTests.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using vfs.core;
+
+namespace vfs.core.tests {
+
+    [TestClass]
+    public class JCDFileStreamTests
+    {
+        private ulong MB50 = 50000000;
+        private int MB5 = 5000000;
+        private int MB1 = 1000000;
+
+        [TestMethod]
+        public void TestWriteRead10MB() {
+            // Set up
+            var stream = CreateJCDAndGetFileStream("test_write_read_10mb", MB50);
+            int bytes = MB5 * 2;
+            var dataIn = GenerateRandomData(bytes, 1);
+            stream.Write(dataIn, 0, bytes);
+
+
+            // Test
+            stream.Seek(0, SeekOrigin.Begin);
+            var dataOut = new byte[bytes];
+            stream.Read(dataOut, 0, bytes);
+            // Verify that all bytes are the same
+            AreEqual(dataIn, dataOut);
+        }
+
+        [TestMethod]
+        public void TestSeekData() {
+            // Set up
+            var stream = CreateJCDAndGetFileStream("test_seek_data", MB50);
+            var bytes = MB5;
+            var dataIn = GenerateRandomData(bytes, 1);
+            stream.Write(dataIn, 0, bytes);
+
+            // Test
+            // Read 4 MB data, starting from 1MB.
+            var dataOut = new byte[bytes - MB1];
+            stream.Seek(MB1, SeekOrigin.Begin);
+            stream.Read(dataOut, 0, bytes - MB1);
+            for (int i = 0; i < bytes - MB1; i += 1) {
+                Assert.AreEqual(dataIn[i + MB1], dataOut[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestOffsetReadData() {
+            // Set up
+            var stream = CreateJCDAndGetFileStream("test_offset_seek_data", MB50);
+            var bytes = MB5;
+            var dataIn = GenerateRandomData(bytes, 1);
+            stream.Write(dataIn, 0, bytes);
+
+            // Test
+            // Read 4 MB data, starting from 1MB.
+            var dataOut = new byte[bytes - MB1];
+            stream.Read(dataOut, MB1, bytes - MB1);
+            for (int i = 0; i < bytes - MB1; i += 1) {
+                Assert.AreEqual(dataIn[i + MB1], dataOut[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestSeekWriteData() {
+            // Set up
+            var stream = CreateJCDAndGetFileStream("test_seek_write_data", MB50);
+            var bytes = MB5;
+            var dataIn = GenerateRandomData(bytes, 15);
+            // Write data 5 megabytes in to file.
+            // We expect the file to automatically expand.
+            stream.Seek(MB5, SeekOrigin.Begin);
+            stream.Write(dataIn, 0, bytes);
+
+            // Test
+            // Read 5 MB data, starting from 5MB.
+            var dataOut = new byte[bytes];
+            stream.Seek(MB5, SeekOrigin.Begin);
+            stream.Read(dataOut, 0, bytes);
+            AreEqual(dataIn, dataOut);
+        }
+
+        [TestMethod]
+        public void TestReadSeekReadSeekRead() {
+            // Set up
+            var stream = CreateJCDAndGetFileStream("test_read_seek_read_seek_read", MB50);
+            var datas = new byte[5][];
+            // Initialize 5 arrays with random data in them.
+            for (int i = 0; i < datas.Length; i += 1) {
+                datas[i] = GenerateRandomData(MB1, i);
+                stream.Write(datas[i], 0, MB1);
+            }
+
+            // Test
+            var dataOut = new byte[MB1];
+            for (int i = datas.Length - 1; i >= 0; i -= 1) {
+                stream.Seek(i * MB1, SeekOrigin.Begin);
+                stream.Read(dataOut, 0, MB1);
+                AreEqual(datas[i], dataOut);
+            }
+        }
+
+        public JCDFileStream CreateJCDAndGetFileStream(string vfsFileName, ulong size) {
+            DeleteFiles(new string[] { vfsFileName });
+            var vfs = JCDFAT.Create(vfsFileName, size);
+            var testFileName = "test";
+            vfs.CreateFile(testFileName, 0, false);
+            return vfs.GetFileStream(testFileName);
+        }
+
+        private byte[] GenerateRandomData(int size, int seed) {
+            var data = new byte[size];
+            var rnd = new Random(seed);
+            rnd.NextBytes(data);
+            return data;
+        }
+
+        private void DeleteFiles(string[] files) {
+            foreach (var file in files) {
+                try {
+                    File.Delete(file);
+                }
+                catch (System.IO.FileNotFoundException) { }
+            }
+        }
+
+        private void AreEqual(byte[] arr1, byte[] arr2) {
+            Assert.AreEqual(arr1.Length, arr2.Length);
+            for (int i = 0; i < arr1.Length; i += 1) {
+                Assert.AreEqual(arr1[i], arr2[i]);
+            }
+        }
+    }
+}
---
+++ vfs/vfs.core/JCDFileStream.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vfs.exceptions;
+
+namespace vfs.core {
+    public class JCDFileStream : Stream {
+        private JCDFile file;
+        private long currentNumBlocks;
+
+        // Implementations.
+        private long length;
+        override public long Length {
+            get {
+                return length;
+            }
+        }
+
+        private long position;
+        override public long Position {
+            get {
+                return position;
+            }
+            set {
+                this.position = value;
+            }
+        }
+
+        // Dummy implementations
+        override public bool CanRead { get { return true; } }
+        override public bool CanSeek { get { return true; } }
+        override public bool CanTimeout { get { return true; } }
+        override public bool CanWrite { get { return true; } }
+        override public int ReadTimeout { get { return 0; } }
+        override public int WriteTimeout { get { return 0; } }
+
+        internal JCDFileStream(JCDFile file) {
+            this.file = file;
+            this.length = (long)file.Size;
+            this.currentNumBlocks = (long)Helpers.ruid(file.Size + 1, JCDFAT.blockSize);
+            this.position = 0L;
+        }
+
+        override public void Flush() {
+            // Will be flushed when the file system is closed.
+        }
+
+        public override int Read(byte[] buffer, int offset, int count) {
+            // Make sure that offset and count don't go beyond bound of file
+            if (offset + count > length) {
+                throw new Exception("File is not that big!");
+            }
+            if (buffer.Length < count) {
+                throw new Exception("Buffer is not big enough!");
+            }
+            var vfs = file.GetContainer();
+            vfs.ReadFile(buffer, (ulong)(position + offset), (ulong)count, file.Entry.FirstBlock);
+            position += offset + count;
+            return buffer.Length;
+        }
+
+        public override void Write(byte[] data, int offset, int count) {
+            // Make sure that we don't try to write more data than we received.
+            if (data.Length < count) {
+                count = data.Length;
+            }
+
+            var vfs = file.GetContainer();
+            // Increase file length in case we're writing beyond #blocks currently allocated.
+            long allocatedBytes = this.currentNumBlocks * JCDFAT.blockSize;
+            long requiredBytes = this.position + offset + count;
+            if (requiredBytes > allocatedBytes) {
+                long expandBytes = requiredBytes - allocatedBytes;
+
+                // Make sure that there's enough free space on the vfs.
+                if ((ulong)expandBytes > vfs.FreeSpace()) {
+                    throw new NotEnoughSpaceException();
+                }
+
+                // Allocate required blocks.
+                long extraBlocksRequired = Helpers.ruid(expandBytes, JCDFAT.blockSize);
+                for (int i = 0; i < extraBlocksRequired; i += 1) {
+                    // TODO: could be implemented more efficiently by doing multiple blocks at the time.
+                    file.ExpandOneBlock();
+                }
+
+                // Update size of file.
+                file.Size = (ulong)(allocatedBytes + expandBytes);
+                currentNumBlocks += extraBlocksRequired;
+            }
+
+            // Update the number of bytes the file spans (which could be less than the
+            // number of blocks * block size.)
+            if ((ulong)requiredBytes > file.Size) {
+                file.Size = (ulong)requiredBytes;
+            }
+
+            length = (long)file.Size;
+
+            vfs.WriteFile(data, position + offset, file.Entry.FirstBlock);
+            position += offset + count;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin) {
+            switch (origin) {
+                case SeekOrigin.Begin:
+                    position = offset;
+                    break;
+                case SeekOrigin.Current:
+                    position += offset;
+                    break;
+                case SeekOrigin.End:
+                    position = length + offset;
+                    // We wait to set the length until user writes beyond file length.
+                    break;
+            }
+            return position;
+        }
+
+        public override void SetLength(long value) {
+            this.length = value;
+        }
+
+        public override void Close() {
+            base.Close();
+        }
+    }
+}
