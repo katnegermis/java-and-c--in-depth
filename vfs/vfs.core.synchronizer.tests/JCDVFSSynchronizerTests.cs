14e34db147c969edf2c7955ffd6da93a7285fcd0
Michael Bang <michael@katnegermis.dk>
2014-05-07 20:25:18 +0200

    Initial commit of vfs.core.synchronizer.

---
+++ vfs/vfs.core.synchronizer.tests/JCDVFSSynchronizerTests.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vfs.common;
+
+namespace vfs.core.synchronizer.tests
+{
+    [TestClass]
+    public class JCDVFSSynchronizerTests
+    {
+        private const uint MB1 = 1000000;
+        private const uint MB5 = MB1 * 5;
+        Type vfsType = typeof(JCDFAT);
+
+        [TestMethod]
+        public void TestSynchronizerCreate() {
+            // Set up
+            var testName = "synchronizer_create";
+            var testFileName = GetTestFileName(testName);
+
+            // Test
+            var sync = JCDVFSSynchronizer.Create(vfsType, testFileName, MB5);
+
+            CleanUp(sync, testName);
+        }
+
+        [TestMethod]
+        public void TestSynchronizerDelete() {
+            // Set up
+            var testName = "synchronizer_delete";
+            var testFileName = GetTestFileName(testName);
+            var sync = JCDVFSSynchronizer.Create(vfsType, testFileName, MB5);
+            Assert.IsTrue(File.Exists(testFileName));
+            sync.Close();
+
+            // Test
+            JCDVFSSynchronizer.Delete(vfsType, testFileName);
+            Assert.IsFalse(File.Exists(testFileName));
+        }
+
+        [TestMethod]
+        public void TestSynchronizerOpen() {
+            // Set up
+            var testName = "synchronizer_open";
+            var testFileName = GetTestFileName(testName);
+            var sync = JCDVFSSynchronizer.Create(vfsType, testFileName, MB5);
+            sync.Close();
+
+            // Test
+            sync = JCDVFSSynchronizer.Open(vfsType, testFileName);
+
+            CleanUp(sync, testName);
+        }
+
+        [TestMethod]
+        public void TestSynchronizerCreateFile() {
+            // Set up
+            var testName = "synchronizer_create_file";
+            var sync = CreateSynchronizer(testName);
+            long fileSize = 5;
+            var filePath = "/file";
+
+            // Test
+            sync.CreateFile(filePath, (ulong)fileSize, false);
+            var stream = sync.GetFileStream(filePath);
+            Assert.AreEqual(fileSize, stream.Length);
+
+            stream.Close();
+            CleanUp(sync, testName);
+        }
+
+        [TestMethod]
+        public void TestSynchronizerCreatefolder() {
+            // Set up
+            var testName = "synchronizer_create_folder";
+            var sync = CreateSynchronizer(testName);
+            var filePath = "/folder";
+
+            // Test
+            sync.CreateDirectory(filePath, false);
+
+            CleanUp(sync, testName);
+        }
+
+        private string GetTestFileName(string testName) {
+            return testName;
+        }
+
+        private JCDVFSSynchronizer CreateSynchronizer(string testName) {
+            return CreateSynchronizer(testName, MB5);
+        }
+
+        private JCDVFSSynchronizer CreateSynchronizer(string testName, uint size) {
+            TestHelpers.DeleteFiles(new string[] { GetTestFileName(testName) });
+            testName = GetTestFileName(testName);
+            return JCDVFSSynchronizer.Create(vfsType, testName, size);
+        }
+
+        private void CleanUp(JCDVFSSynchronizer sync, string testName) {
+            sync.Close();
+            TestHelpers.DeleteFiles(new string[] { GetTestFileName(testName) });
+        }
+    }
+}
---
+++ vfs/vfs.core.synchronizer/JCDVFSSynchronizer.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vfs.core;
+
+namespace vfs.core.synchronizer
+{
+    public class JCDVFSSynchronizer : IJCDBasicVFS
+    {
+        public event AddFileEventHandler FileAdded;
+        public event DeleteFileEventHandler FileDeleted;
+        public event MoveFileEventHandler FileMoved;
+        public event ModifyFileEventHandler FileModified;
+        private IJCDBasicVFS vfs;
+
+        /// <summary>
+        /// Event to be called every time a new file is added to the file system.
+        /// </summary>
+        /// <param name="path">Path of the newly added file.</param>
+        internal void OnFileAdded(string path) {
+            if (FileAdded != null) {
+                FileAdded(path);
+            }
+        }
+
+        /// <summary>
+        /// Event to be called every time a file is deleted from the file system.
+        /// </summary>
+        /// <param name="path">Path of the deleted file.</param>
+        internal void OnFileDeleted(string path) {
+            if (FileDeleted != null) {
+                FileDeleted(path);
+            }
+        }
+
+        /// <summary>
+        /// Event to be called every time a file is moved or renamed on the file system.
+        /// </summary>
+        /// <param name="oldPath">File's previous (old) path.</param>
+        /// <param name="newPath">File's new (current) path.</param>
+        internal void OnFileMoved(string oldPath, string newPath) {
+            if (FileMoved != null) {
+                FileMoved(oldPath, newPath);
+            }
+        }
+
+        /// <summary>
+        /// Event to be called every time a file is modified.
+        /// This does NOT include file resizing!
+        /// </summary>
+        /// <param name="path">File's path.</param>
+        /// <param name="offset">Offset in to file where the data was written.</param>
+        /// <param name="data">Data that was written.</param>
+        internal void OnFileModified(string path, long startByte, byte[] data) {
+            if (FileModified != null) {
+                FileModified(path, startByte, data);
+            }
+        }
+
+        public JCDVFSSynchronizer(IJCDBasicVFS vfs) {
+            this.vfs = vfs;
+
+            FileModified += OnFileModified;
+            FileAdded += OnFileAdded;
+            FileDeleted += OnFileDeleted;
+            FileMoved += OnFileMoved;
+        }
+
+        /// <summary>
+        /// Create a new VFS-file.
+        /// </summary>
+        /// <param name="hfsPath">The path of the file on the hard file system</param>
+        /// <param name="size">The size of the new vfs</param>
+        /// <returns>An object of an implementation of the IJCDBasicVFS interface</returns>
+        /// <exception cref="System.IO.DirectoryNotFoundException"
+        // - too little space available on HFS
+        // - invalid path string (file name too long/invalid characters).
+        // - no permissions to write on HFS.
+        public static JCDVFSSynchronizer Create(Type vfsType, string hfsPath, ulong size) {
+            var vfs = (IJCDBasicVFS)IJCDBasicTypeCallMethod(vfsType, "Create", new object[] { hfsPath, size });
+            return new JCDVFSSynchronizer(vfs);
+        }
+
+        /// <summary>
+        /// Delete an unmounted VFS from HFS.
+        /// </summary>
+        /// <param name="hfsPath">The path of the file on the host file system</param>
+        /// <exception cref="System.IO.DirectoryNotFoundException"
+        // - VFS is mounted.
+        public static void Delete(Type vfsType, string hfsPath) {
+            IJCDBasicTypeCallMethod(vfsType, "Delete", new object[] { hfsPath });
+        }
+
+        /// <summary>
+        /// Mount an existing VFS-file.
+        /// </summary>
+        /// <param name="hfsPath"> The path of the file on the host file system</param>
+        /// <returns>An object of an implementation of the IJCDBasicVFS interface</returns>
+        /// <exception cref="System.IO.DirectoryNotFoundException"></exception>
+        // - VFS file already mounted.
+        // - file is not VFS type.
+        public static JCDVFSSynchronizer Open(Type vfsType, string hfsPath) {
+            var vfs = (IJCDBasicVFS)IJCDBasicTypeCallMethod(vfsType, "Open", new object[] { hfsPath });
+            return new JCDVFSSynchronizer(vfs);
+
+        }
+
+        /// <summary>
+        /// Unmount a mounted VFS.
+        /// </summary>
+        // - No VFS mounted
+        public void Close() {
+            vfs.Close();
+        }
+
+        /// <summary>
+        /// Get total size of a mounted VFS.
+        /// </summary>
+        /// <returns>Size of mounted VFS.</returns>
+        // - No VFS mounted
+        public ulong Size() {
+            return vfs.Size();
+        }
+
+        /// <summary>
+        /// Get total amount of occupied space on a mounted VFS.
+        /// </summary>
+        /// <returns>Amount of occupied space.</returns>
+        /// <remarks>It should hold that OccupiedSpace + UnoccupiedSpace == Size</remarks>
+        // - No VFS mounted
+        public ulong OccupiedSpace() {
+            return vfs.OccupiedSpace();
+        }
+
+        /// <summary>
+        /// Get total amount of unoccupied space on a mounted VFS.
+        /// </summary>
+        /// <returns>Amount of unoccupied space.</returns>
+        /// <remarks>It should hold that OccupiedSpace + UnoccupiedSpace == Size</remarks>
+        /// - No VFS mounted
+        public ulong FreeSpace() {
+            return vfs.FreeSpace();
+        }
+
+        /// <summary>
+        /// Create a directory on a mounted VFS.
+        /// Optionally create parents (like mkdir -p).
+        /// </summary>
+        /// <param name="vfsPath"></param>
+        /// <param name="createParents"></param>
+        // Exceptions:
+        // - no such path (createParents == false).
+        // - too little space available on VFS.
+        // - invalid path string (file name too long/invalid characters).
+        public void CreateDirectory(string vfsPath, bool createParents) {
+            vfs.CreateDirectory(vfsPath, createParents);
+        }
+
+        /// <summary>
+        /// Create a file on a mounted VFS of the given size.
+        /// Optionally create parents (like mkdir -p).
+        /// </summary>
+        /// <param name="vfsPath"></param>
+        /// <param name="createParents"></param>
+        // Exceptions:
+        // - no such path (createParents == false).
+        // - too little space available on VFS.
+        // - invalid path string (file name too long/invalid characters).
+        public void CreateFile(string vfsPath, ulong size, bool createParents) {
+            vfs.CreateFile(vfsPath, size, createParents);
+        }
+
+        /// <summary>
+        /// Import a file or directory from HFS to a mounted VFS.
+        /// </summary>
+        // Exceptions:
+        // - no such path on HFS.
+        // - no such path on VFS.
+        // - invalid HFS path string (file name too long/invalid characters).
+        // - invalid VFS path string (file name too long/invalid characters).
+        public void ImportFile(string hfsPath, string vfsPath) {
+            vfs.ImportFile(hfsPath, vfsPath);
+        }
+
+        /// <summary>
+        /// Export a file or directory from a mounted VFS to HFS.
+        /// </summary>
+        // Exceptions:
+        // - no such path on VFS.
+        // - no such path on HFS.
+        // - too little space available on HFS.
+        // - invalid HFS path string (file name too long/invalid characters).
+        // - invalid VFS path string (file name too long/invalid characters).
+        public void ExportFile(string vfsPath, string hfsPath) {
+            vfs.ExportFile(vfsPath, hfsPath);
+        }
+
+        /// <summary>
+        /// Delete a file or directory on a mounted VFS.
+        /// </summary>
+        // Exceptions:
+        // - invalid path string (file name too long/invalid characters).
+        // - no such path.
+        // - path points to a directory (recursive == false).
+        public void DeleteFile(string vfsPath, bool recursive) {
+            vfs.DeleteFile(vfsPath, recursive);
+        }
+
+        ///<summary>
+        /// Rename file or directory on a mounted VFS.
+        /// </summary>
+        // Exceptions:
+        // - invalid path string (file name too long/invalid characters).
+        // - invalid file name (too long/invalid characters).
+        // - no such file on VFS.
+        public void RenameFile(string vfsPath, string newName) {
+            vfs.RenameFile(vfsPath, newName);
+        }
+
+        /// <summary>
+        /// Move file or directory on a mounted VFS.
+        /// </summary>
+        // Exceptions:
+        // - invalid path string (file name too long/invalid characters).
+        // - invalid file name (too long/invalid characters).
+        // - no such file on VFS.
+        public void MoveFile(string vfsPath, string newVfsPath) {
+            vfs.MoveFile(vfsPath, newVfsPath);
+        }
+
+        /// <summary>
+        /// Copy file or directory on a mounted VFS.
+        /// </summary>
+        // Exceptions:
+        // - invalid path string (file name too long/invalid characters).
+        // - invalid file name (too long/invalid characters).
+        // - no such file on VFS.
+        public void CopyFile(string vfsPath, string newVfsPath) {
+            vfs.CopyFile(vfsPath, newVfsPath);
+        }
+
+        /// <summary>
+        /// List contents of a directory.
+        /// </summary>
+        /// <returns>List of directories and files contained in vfsPath.</returns>
+        // Exceptions:
+        // - path points to a file (not directory).
+        public JCDDirEntry[] ListDirectory(string vfsPath) {
+            return vfs.ListDirectory(vfsPath);
+        }
+
+        public void SetCurrentDirectory(string vfsPath) {
+            vfs.SetCurrentDirectory(vfsPath);
+        }
+
+        public string GetCurrentDirectory() {
+            return vfs.GetCurrentDirectory();
+        }
+
+        public JCDFileStream GetFileStream(string vfsPath) {
+            return vfs.GetFileStream(vfsPath);
+        }
+
+        internal static object IJCDBasicTypeCallMethod(Type type, string methodName, object[] args) {
+            if (type.GetInterface("IJCDBasicVFS") == null) {
+                throw new Exception("Can only be used with objects that implement IJCDBasicVFS");
+            }
+
+            var method = type.GetMethod(methodName);
+            return (IJCDBasicVFS)method.Invoke(null, args);
+        }
+    }
+}
