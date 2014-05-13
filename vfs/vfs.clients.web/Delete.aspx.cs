10ca6b0977b0a2120ff605c0957047810813a23d
Vladimir Grozman <xogede@gmail.com>
2014-05-13 20:27:27 +0200

    added missing files

---
+++ vfs/vfs.clients.web/Delete.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace vfs.clients.web {
+    public partial class Delete : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            Master.checkSession();
+        }
+
+        public void deleteVFS(object sender, EventArgs e) {
+            Global.vfsSession.DeleteVFS();
+            Global.TerminateSession();
+            Master.checkSession();
+        }
+
+        public void cancel(object sender, EventArgs e) {
+            Response.Redirect("~/");
+        }
+    }
+}
